6ad3b81280bbebb4adf91c273cd87f7849df6230
nikalosa <nikalosaberidze@gmail.com>
2020-09-02 02:06:56 +0400

    Add merger function for user statistics

---
+++ functions/statistics/user/main.py
@@ -0,0 +1,31 @@
+import json
+import requests
+
+
+USERS_URL = 'https://vikvok-anldg2io3q-ew.a.run.app/users/{}'
+STATISTICS_USER_URL = "https://vikvok-anldg2io3q-ew.a.run.app/statistics/user/{}"
+ORIGINAL_VOICES_URL = 'https://vikvok-anldg2io3q-ew.a.run.app/originalvoices/{}'
+
+
+def original_voice_comments(request):
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+    if request_json and 'userId' in request_json:
+        user_id = request_json['userId']
+    elif request_args and 'userId' in request_args:
+        user_id = request_args['userId']
+    else:
+        return "userId not found!"
+
+    statistics_json = requests.get(STATISTICS_USER_URL.format(user_id)).json()
+
+    for key in ['maxScore', 'timeScore']:
+        for dic in statistics_json[key]:
+            voice_id = dic['voiceId']
+            del dic['voiceId']
+            dic['voice'] = requests.get(ORIGINAL_VOICES_URL.format(voice_id)).json()
+
+    for i, voice_id in enumerate(statistics_json['popular_voices']):
+        statistics_json['popular_voices'][i] = requests.get(ORIGINAL_VOICES_URL.format(voice_id)).json()
+
+    return json.dumps(statistics_json)
