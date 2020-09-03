3b3c2340cee32e15bb5c76fa16e4cd347c643e1f
nikalosa <nikalosaberidze@gmail.com>
2020-09-03 22:16:43 +0400

    variable names changed

--- functions/statistics/voice/id/main.py
+++ functions/statistics/voice/id/main.py
@@ -12,28 +12,28 @@ def one_voice_statistics(request):
     request_json = request.get_json(silent=True)
     request_args = request.args
     if request_json and "originalVoiceId" in request_json:
-        voiceId = request_json["originalVoiceId"]
+        voice_id = request_json["originalVoiceId"]
     elif request_args and "originalVoiceId" in request_args:
-        voiceId = request_args["originalVoiceId"]
+        voice_id = request_args["originalVoiceId"]
     else:
-        return (json.dumps({"error": "Missing parameter: originalVoiceId"}), 422, {})
+        return json.dumps({"error": "Missing parameter: originalVoiceId"}), 422, {}
 
     # 2. Fetch Statistics for given voice
     try:
-        url = STATISTICS_VOICE_URL.format(voiceId=voiceId)
+        url = STATISTICS_VOICE_URL.format(voiceId=voice_id)
         statistics_json = requests.get(url).json()
     except requests.exceptions.RequestException as err:
-        return (json.dumps({"API Call Path": url, "Error": err}), 500, {})
+        return json.dumps({"API Call Path": url, "Error": err}), 500, {}
 
     # 3. Fetch Maxscorer users
     for elem in statistics_json["maxScorers"]:
         try:
-            userId = elem["UserId"]
-            url = USERS_URL.format(userId=userId)
+            user_id = elem["UserId"]
+            url = USERS_URL.format(userId=user_id)
             user = requests.get(url).json()
             elem["user"] = user
         except requests.exceptions.RequestException as err:
-            return (json.dumps({"API Call Path": url, "Error": err}), 500, {})
+            return json.dumps({"API Call Path": url, "Error": err}), 500, {}
 
     # 4. Return Data in JSON
     return json.dumps(statistics_json, indent=4, sort_keys=True, default=str)
