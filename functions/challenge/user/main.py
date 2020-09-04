67ff0cb23d0b0ce80d972ca5fac34bfa7241eefb
nikalosa <nikalosaberidze@gmail.com>
2020-09-04 05:53:54 +0400

    Fix naming of arguments

--- functions/challenge/user/main.py
+++ functions/challenge/user/main.py
@@ -3,7 +3,7 @@ import requests
 
 API_PATH = "https://vikvok-anldg2io3q-ew.a.run.app"
 
-CHALLENGE = API_PATH + "/challenges/{userId}"
+CHALLENGE = API_PATH + "/challenges/{receiverUserId}"
 USER = API_PATH + "/users/{userId}"
 
 
@@ -11,15 +11,15 @@ def merge_challenge_user(request):
     # 1. Get ID from request
     request_json = request.get_json(silent=True)
     request_args = request.args
-    if request_json and "userId" in request_json:
-        user_id = request_json["userId"]
-    elif request_args and "userId" in request_args:
-        user_id = request_args["userId"]
+    if request_json and "receiverUserId" in request_json:
+        user_id = request_json["receiverUserId"]
+    elif request_args and "receiverUserId" in request_args:
+        user_id = request_args["receiverUserId"]
     else:
         return json.dumps({"error": "Missing parameter: userId"}), 422, {}
 
     # 2. Get Recorded Voice
-    url = CHALLENGE.format(userId=user_id)
+    url = CHALLENGE.format(receiverUserId=user_id)
     try:
         challenges = requests.get(url).json()
     except requests.exceptions.RequestException as err:
