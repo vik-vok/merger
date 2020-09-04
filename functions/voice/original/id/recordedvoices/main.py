0c1716576a21e438bfd1e0aebea1d46870a30c1f
nikalosa <nikalosaberidze@gmail.com>
2020-09-04 07:27:25 +0400

    Add Score

--- functions/voice/original/id/recordedvoices/main.py
+++ functions/voice/original/id/recordedvoices/main.py
@@ -1,10 +1,33 @@
 import json
 import requests
+from google.cloud import bigquery
 
 RECORDED_VOICES_URL = (
     "https://vikvok-anldg2io3q-ew.a.run.app/recordedvoices/original/{}"
 )
 USERS_URL = "https://vikvok-anldg2io3q-ew.a.run.app/users/{}"
+
+
+def get_score(recordedVoiceId):
+    client = bigquery.Client("speech-similarity")
+
+    query_job = client.query(
+        """
+            select
+                Score
+            from 
+                statistics.recorded_voices 
+            where
+                RecordedVoiceId = '{recordedVoiceId}'
+        """.format(
+            recordedVoiceId=recordedVoiceId
+        )
+    )
+
+    users_tried = query_job.result()
+
+    for row in users_tried:
+        return row.Score
 
 
 def original_voice_recorded_voices(request):
@@ -25,5 +48,6 @@ def original_voice_recorded_voices(request):
         user = requests.get(USERS_URL.format(user_id)).json()
         del voices_json[i]["userId"]
         voices_json[i]["user"] = user
+        voices_json[i]["score"] = get_score(voices_json[i]["recordedVoiceId"])
 
     return json.dumps(voices_json)
